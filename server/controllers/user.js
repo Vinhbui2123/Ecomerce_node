dd0c72a066a252f1f0cc49231307819c3f9371e2
Vinh Bui <vindepzai004@gmail.com>
2025-03-09 21:21:41 +0700

    feat(project): login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,10 +9,39 @@ const register = asyncHandler(async (req, res) => {
       message: "All fields are required",
     })
   }
-  const response = await User.create(req.body)
-  return res.status(200).json({
-    success: response ? true : false,
-    response,
-  })
+  const user = await User.findOne({ email })
+  if (user) {
+    throw new Error("User already exists")
+  } else {
+    const newUser = await User.create(req.body)
+    return res.status(200).json({
+      success: newUser ? true : false,
+      mes: newUser ? "User created successfully" : "something went wrong",
+    })
+  }
 })
-module.exports = { register }
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Missing output",
+    })
+  }
+  const response = await User.findOne({ email })
+  if (response && (await response.isCorrectPassword(password))) {
+    const { password, role, ...userData } = response.toObject()
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      userData,
+    })
+  } else {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid credentials",
+    })
+  }
+})
+
+module.exports = { register, login }
