71bc14fd7199ee4c10faada5d85a719dd325c842
Vinh Bui <vindepzai004@gmail.com>
2025-03-29 17:42:13 +0700

    feat(project): add Tailwind CSS line-clamp plugin and update BestSeller component with new product display logic

--- client/src/components/BestSeller.js
+++ client/src/components/BestSeller.js
@@ -29,25 +29,34 @@ const BestSeller = () => {
   const [bestSeller, setbestSeller] = useState(null)
   const [newProducts, setNewProducts] = useState(null)
   const [activedTab, setactiveTab] = useState(1)
+  const [products, setproducts] = useState(null)
   const fetchProducts = async () => {
     const response = await Promise.all([
       apiGetProducts({ sort: "-sold" }),
       apiGetProducts({ sort: "-createdAt" }),
     ])
-    if (response[0]?.success) setbestSeller(response[0].products)
+    if (response[0]?.success) {
+      setbestSeller(response[0].products)
+      setproducts(response[0].products)
+    }
     if (response[1]?.success) setNewProducts(response[1].products)
+    setproducts(response[0].products)
   }
 
   useEffect(() => {
     fetchProducts()
   }, [])
+  useEffect(() => {
+    if (activedTab === 1) setproducts(bestSeller)
+    if (activedTab === 2) setproducts(newProducts)
+  }, [activedTab])
   return (
     <div>
-      <div className="flex text-[20px] gap-8 pb-4 border-main border-b-2">
+      <div className="flex text-[20px] ml-[-32px]">
         {tabs.map((el) => (
           <span
             key={el.id}
-            className={`font-semibold capitalize border-r cursor-pointer text-gray-400 ${
+            className={`font-semibold capitalize px-8 border-r cursor-pointer text-gray-400 ${
               activedTab === el.id ? "text-gray-900" : ""
             }`}
             onClick={() => setactiveTab(el.id)}
@@ -56,10 +65,14 @@ const BestSeller = () => {
           </span>
         ))}
       </div>
-      <div className="mt-4">
+      <div className="mt-4 mx-[-10px] border-main border-t-2 pt-4">
         <Slider {...settings}>
-          {bestSeller?.map((el) => (
-            <Product key={el.id} productData={el} />
+          {products?.map((el) => (
+            <Product
+              key={el.id}
+              productData={el}
+              isNew={activedTab === 1 ? false : true}
+            />
           ))}
         </Slider>
       </div>
--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,9 +1,39 @@
 import React from "react"
+import { formatCurrency } from "../ultils/helper"
+import label from "../assets/label.png"
+import label1 from "../assets/label1.png"
 
-const Product = ({ productData }) => {
+const Product = ({ productData, isNew }) => {
   return (
-    <div className="w-1/3">
-      <img src={productData?.images[0] || ""} alt=""></img>
+    <div className="w-full text-base px-[10px]">
+      <div className="w-full border p-[15px] flex flex-col gap-2 items-center justify-center rounded-lg hover:shadow-md transition-all duration-200 ease-in-out">
+        <div className="w-full relative">
+          <img
+            src={
+              productData?.thumb ||
+              "https://www.shutterstock.com/image-vector/missing-picture-page-website-design-600nw-1552421075.jpg"
+            }
+            alt=""
+            className="w-[243px] h-[243px] object-cover"
+          />
+          <img
+            src={isNew ? label : label1}
+            alt=""
+            className="absolute top-[-32px] left-[-42px] w-[120px] object-contain"
+          />
+          <span
+            className={`font-bold top-[-10px] left-[-12px] text-white absolute ${
+              isNew ? "" : "text-sm"
+            }`}
+          >
+            {isNew ? "New" : "Trending"}
+          </span>
+        </div>
+        <div className="flex flex-col gap-1 mt-[15px] items-start w-full">
+          <span className="line-clamp-1">{productData?.title}</span>
+          <span>{`${formatCurrency(productData?.price)} VND`}</span>
+        </div>
+      </div>
     </div>
   )
 }
