15f0ebd4e50af27bf63ae3d45c0f9a2010132f66
Vinh Bui <vindepzai004@gmail.com>
2025-03-30 16:56:46 +0700

    feat(DealDaily): add DealDaily component with countdown timer and product fetching functionality; include CountDown component

--- client/src/ultils/helper.js
+++ client/src/ultils/helper.js
@@ -9,12 +9,13 @@ export const createSlug = (string) =>
     .split(" ")
     .join("-")
 export const formatCurrency = (number) =>
-  Number(number.toFixed(1)).toLocaleString()
-export const renderStarFromNumber = (number) => {
+  Number(number?.toFixed(1)).toLocaleString()
+export const renderStarFromNumber = (number, size) => {
   if (!Number(number)) return
   const stars = []
-  for (let i = 0; i < +number; i++) stars.push(<AiFillStar color="gold" />)
-  for (let i = 5; i > +number; i--) stars.push(<AiOutlineStar color="gray" />)
-
+  for (let i = 0; i < +number; i++)
+    stars.push(<AiFillStar color="gold" size={size || 16} />)
+  for (let i = 5; i > +number; i--)
+    stars.push(<AiOutlineStar color="gray" size={size || 16}/>)
   return stars
 }
